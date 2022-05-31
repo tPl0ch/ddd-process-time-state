fb7a19454d365b03f38248f5ca7a806dc51d166f
Thomas Ploch <thomas.ploch@flixbus.com>
2022-05-31 12:32:41 +0200

    Finalized guards

--- src/main/scala/machines.scala
+++ src/main/scala/machines.scala
@@ -1,25 +1,51 @@
 package org.tp.process_time_state
 
-import cats.{ FlatMap, Functor, Id as CatsId }
+import cats.data.Validated.*
+import cats.data.{ NonEmptyChain, NonEmptyList, ValidatedNec }
+import cats.{ Applicative, ApplicativeError, FlatMap, Functor, Id as CatsId }
+import cats.implicits.*
 
 object machines {
 
+  type ErrorF[F[_]] = ApplicativeError[F, NonEmptyList[Throwable]]
+
+  opaque type GuardF[C, S, E <: Throwable] = C => S => ValidatedNec[E, Unit]
+  opaque type BehaviorF[F[_], C, S]        = C => S => F[S]
+
+  object BehaviorF {
+    def apply[F[_]: ErrorF, C, S, E <: Throwable](b: BehaviorF[F, C, S])(
+        guards: List[GuardF[C, S, E]],
+    ): BehaviorF[F, C, S] =
+      (c: C) =>
+        (s: S) => {
+          guards.map(f => f(c)(s)).sequence match
+            case Valid(_)                       => b(c)(s)
+            case Invalid(nec: NonEmptyChain[E]) => nec.toNonEmptyList.raiseError[F, S]
+        }
+  }
+
+  type OutputF[F[_], C, S, E] = C => S => F[E]
+
   opaque type StateF[F[_], S, E] = S => F[(S, E)]
-  type State[S, E]               = StateF[CatsId, S, E]
 
   opaque type MachineF[F[_], C, S, E] = C => StateF[F, S, E]
 
   object StateF {
     def apply[F[_], S, E](f: S => F[(S, E)]): StateF[F, S, E] = f
 
-    def apply[F[_]: FlatMap: Functor, S, E](b: S => F[S])(o: S => F[E]): StateF[F, S, E] =
-      (s: S) => FlatMap[F].flatMap(b(s))(ss => Functor[F].map(o(s))(e => (ss, e)))
-
-    def unit[F[_]: Functor, S, E](fe: F[E]): StateF[F, S, E] =
-      (s: S) => Functor[F].map(fe)((s, _))
+    def apply[F[_]: FlatMap: Functor, S, E](
+        behaviors: S => F[S],
+    )(outputs: S => F[E]): StateF[F, S, E] =
+      (currentState: S) =>
+        FlatMap[F].flatMap(behaviors(currentState))(ss =>
+          Functor[F].map(outputs(currentState))(e => (ss, e)),
+        )
   }
 
   object MachineF {
-    def apply[F[_], C, S, E](b: C => S => F[S])(o: C => S => F[E]): MachineF[F, C, S, E] = ???
+    def apply[F[_]: FlatMap: Functor, C, S, E](behaviors: BehaviorF[F, C, S])(
+        outputs: OutputF[F, C, S, E],
+    ): MachineF[F, C, S, E] =
+      (command: C) => StateF.apply(behaviors(command))(outputs(command))
   }
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,90 +1,10 @@
 package org.tp.process_time_state
 
-type Id[A] = A
-
-type LabelF[F[_], C, S] = (C, F[S])
-type Label[C, S]        = LabelF[Id, C, S]
-
-type BehaviorsF[F[_], C, S] = PartialFunction[LabelF[F, C, S], S]
-type Behaviors[C, S]        = PartialFunction[Label[C, S], S]
-
-type IsFinalF[F[_], S] = F[S] => Boolean
-type IsFinal[S]        = IsFinalF[Id, S]
-
-type MooreF[F[_], S, E] = PartialFunction[LabelF[F, _, S], E]
-type Moore[S, E]        = PartialFunction[Label[_, S], E]
-
-type MealyF[F[_], C, S, E] = PartialFunction[LabelF[F, C, S], E]
-type Mealy[C, S, E]        = PartialFunction[Label[C, S], E]
-
-type OutputsF[F[_], C, S, E] = MooreF[F, S, E] | MealyF[F, C, S, E]
-type Outputs[C, S, E]        = Moore[S, E] | Mealy[C, S, E]
-
-opaque type MachineF[F[_], C, S] = (BehaviorsF[F, C, S], IsFinalF[F, S])
-type Machine[C, S]               = (BehaviorsF[Id, C, S], IsFinalF[Id, S])
-
-opaque type TransducerF[F[_], C, S, E] = (BehaviorsF[F, C, S], OutputsF[F, C, S, E], IsFinalF[F, S])
-type Transducer[C, S, E] = (BehaviorsF[Id, C, S], OutputsF[Id, C, S, E], IsFinalF[Id, S])
-
-extension [C, S](underlying: Machine[C, S])
-  def behaviors: Behaviors[C, S] = underlying._1
-  def isFinal: IsFinal[S]        = underlying._2
-
-  def traverse(commands: Seq[C])(initialState: S): LazyList[(C, S)] =
-    commands
-      .foldLeft((initialState, LazyList.empty[(C, S)])) { (sa, c) =>
-        val (s, acc) = sa
-        if isFinal(s)
-        then (s, acc)
-        else {
-          val newState = behaviors(c, s)
-          (newState, acc.appended((c, newState)))
-        }
-      }
-      ._2
-
-  def toFST[E](outputs: Outputs[C, S, E]): Transducer[C, S, E] =
-    val (b, f) = underlying
-    (b, outputs, f)
-
-extension [C, S, E](underlying: Transducer[C, S, E])
-  def behaviors: Behaviors[C, S] = underlying._1
-  def outputs: Outputs[C, S, E]  = underlying._2
-  def isFinal: IsFinal[S]        = underlying._3
-
-  def traverse(commands: Seq[C])(initialState: S): LazyList[E] =
-    underlying.toFSM.traverse(commands)(initialState).map(underlying._2)
-
-  def toFSM: Machine[C, S] =
-    val (b, _, f) = underlying
-    (b, f)
-
-object FSM:
-
-  def unit[C, S](
-      behaviors: Behaviors[C, S],
-      isFinal: IsFinal[S],
-  ): Machine[C, S] =
-    (behaviors, isFinal)
-
-object FST:
-
-  def unit[C, S, E](
-      behaviors: Behaviors[C, S],
-      outputs: Outputs[C, S, E],
-      isFinal: IsFinal[S],
-  ): Transducer[C, S, E] =
-    (behaviors, outputs, isFinal)
-
 import Commands.*
 import Events.*
 import States.*
 
 import java.util.UUID
-
-type Action    = Behaviors[Commands, States]
-type Output    = Moore[States, Events]
-type Aggregate = Machine[Commands, States]
 
 final case class UserId(id: UUID)     extends AnyVal
 final case class Email(value: String) extends AnyVal
@@ -112,6 +32,7 @@ enum States:
   )
   case Active(userId: UserId, email: Email)
   case Deleted(userId: UserId)
+/*
 
 val register: Action = { case (c: Register, _: PotentialCustomer) =>
   AwaitingRegistrationConfirmation(c.id, c.email, Token("token"))
@@ -162,3 +83,4 @@ def main(): Unit = {
 
   userRegistrationWithEvents.traverse(commands)(PotentialCustomer()).foreach(println)
 }
+ */
