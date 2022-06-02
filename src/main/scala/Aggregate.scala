7fa808f81c1177cb48a5da7a3cac55302b886d95
Thomas Ploch <thomas.ploch@flixbus.com>
2022-06-02 17:21:27 +0200

    More documentation

--- src/main/scala/Aggregate.scala
+++ src/main/scala/Aggregate.scala
@@ -1,21 +1,31 @@
 package org.tp
 package process_time_state
 
-import cats.data.Validated.*
-import cats.data.{ NonEmptyChain, NonEmptyList, Validated, ValidatedNel }
-import cats.{ Applicative, ApplicativeError, FlatMap, Functor }
-import cats.implicits.*
-
-import scala.annotation.targetName
+import cats.data.NonEmptyList
 
 trait Aggregate[F[_]] extends Transitions[F] with Events[F] with States[F] {
+
+  /** The type of the Aggregate's identity */
   type ID
+
+  /** The Command alphabet type as a subtype of HasIdentity[ID] */
   type C <: HasIdentity[ID]
+
+  /** The State alphabet type as a subtype of HasIdentity[ID] */
   type S <: HasIdentity[ID]
+
+  /** The Event alphabet type as a subtype of HasIdentity[ID] */
   type E <: HasIdentity[ID]
+
+  /** The Domain Error type as a subtype of DomainError */
   type EE <: DomainError
+
+  /** We are accumulating domain errors in a NonEmptyList */
   type NEC = NonEmptyList[EE]
 
-  type LabelIn   = (C, S)
+  /** The (Command, State) input label */
+  type LabelIn = (C, S)
+
+  /** The (State, Event) output label lifted into Effect F */
   type LabelOutF = F[(S, E)]
 }
