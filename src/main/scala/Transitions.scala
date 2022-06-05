97646336ff3692faee97248d4f5ce125006c9b63
Thomas Ploch <thomas.ploch@flixbus.com>
2022-06-05 11:09:07 +0200

    Docs for Transitions

--- src/main/scala/Transitions.scala
+++ src/main/scala/Transitions.scala
@@ -8,22 +8,49 @@ import cats.implicits.*
 
 import scala.annotation.targetName
 
+/** The Transitions[F] trait provides the Aggregate instance with defining state transitions and
+  * invariants.
+  */
 trait Transitions[F[_]] { self: Aggregate[F] =>
-  type Invariant   = PartialFunction[LabelIn, ValidatedNel[EE, Unit]]
-  type Transition  = PartialFunction[LabelIn, F[S]]
+
+  /** An Invariant is a partial function that either gives a valid Unit value, or an accumulating
+    * error structure
+    */
+  type Invariant = PartialFunction[LabelIn, ValidatedNel[EE, Unit]]
+
+  /** A Transition is a partial function (not all combinations of commands and states need to have a
+    * transition defined) that maps an input label to a state.
+    */
+  type Transition = PartialFunction[LabelIn, F[S]]
+
+  /** A TransitionF is just an alias to Kleisli[F, LabelIn, S]. Kleisli structures are
+    * representations of functions C => F[S] - exactly what we need to model a function that maps a
+    * Command to a State within the context of F.
+    */
   type TransitionF = Kleisli[F, LabelIn, S]
 
+  /** This abstract method needs to provide all the state transitions supported by the Aggregate.
+    * You can use the `mkTransitionF` helper method to easily lift the composed partial functions
+    * into the Kleisli data structure.
+    *
+    * @see
+    *   mkTransitionF
+    */
   def transitions: TransitionF
 
-  final case class TransitionNotDefined(l: LabelIn) extends DomainError {
+  /** This error is indicated when there is no TransitionF for a LabelIn.
+    */
+  private final case class TransitionNotDefined(l: LabelIn) extends DomainError {
     override def msg: String = s"Transition is not defined for command ${l._1} and state ${l._2}"
   }
 
-  final case class InvariantCannotBeChecked(l: LabelIn) extends DomainError {
+  /** This error is indicated when there is no Invariant for a LabelIn on a specific TransitionF */
+  private final case class InvariantCannotBeChecked(l: LabelIn) extends DomainError {
     override def msg: String =
       s"An Invariant could not be checked for command ${l._1} and state ${l._2}"
   }
 
+  /** Lifts a Transition into TransitionF */
   final protected def mkTransitionF(
       transitionsToBeLifted: Transition,
   )(using
@@ -58,6 +85,7 @@ trait Transitions[F[_]] { self: Aggregate[F] =>
 
   extension (transition: Transition)
     @targetName("withGuards")
+    /** Guards a Transition with a List of Invariants */
     def <<<(
         invariants: List[Invariant],
     )(using
@@ -68,6 +96,7 @@ trait Transitions[F[_]] { self: Aggregate[F] =>
     )
 
     @targetName("withGuard")
+    /** Guards a Transition with single Invariant */
     def <<(
         invariant: Invariant,
     )(using
